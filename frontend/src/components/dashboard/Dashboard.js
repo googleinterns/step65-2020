fd553d5d6ebca78aed63f2597ae441167bf20c0d
Indhu Gunda <43057952+indhu-gunda@users.noreply.github.com>
2020-06-25 14:02:17 -0500

    Add shared UI components: Home & Navigation Sidebar components (#1)
    * Add Gallery Preview on Home page
    
    * Format GalleryPreview and add multiple to Home
    
    * Update sidebar with navigation items
    
    * Add new line at end of files
    
    * Fix styling issues

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -10,7 +10,6 @@ import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import Container from '@material-ui/core/Container';
@@ -18,13 +17,8 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import AvatarHeader from './AvatarHeader';
 import NavigationItems from './NavigationItems';
+import GalleryPreview from './GalleryPreview';
 
 const drawerWidth = 240;
 
@@ -138,17 +132,8 @@ export default function Dashboard() {
             </IconButton>
           </div>
           <Divider />
-          <AvatarHeader />
           <NavigationItems />
           <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
         </Drawer>
         <main
           className={clsx(classes.content, {
@@ -159,18 +144,34 @@ export default function Dashboard() {
           <Switch>
             <Route exact path="/">
               <Container>
+                <GalleryPreview name="Museum Gallery" link="/museum-gallery"/>
+                <GalleryPreview name="User Uploads Gallery" link="/user-uploads-gallery"/>
+              </Container>
+            </Route>
+            <Route exact path="/museum-gallery">
+              <Container>
                 <Typography variant="h3" gutterBottom>
-                  Home
+                  Museum Gallery
                 </Typography>
                 <Typography variant="body1" gutterBottom>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
                 </Typography>
               </Container>
             </Route>
-            <Route exact path="/about">
+            <Route exact path="/user-uploads-gallery">
               <Container>
                 <Typography variant="h3" gutterBottom>
-                  About
+                  User Uploads Gallery
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                </Typography>
+              </Container>
+            </Route>
+            <Route exact path="/upload-artwork">
+              <Container>
+                <Typography variant="h3" gutterBottom>
+                  Upload Artwork
                 </Typography>
                 <Typography variant="body1" gutterBottom>
                   Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
---
+++ frontend/src/components/dashboard/GalleryPreview.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import ImgMediaCard from './ImgMediaCard';
+
+const useStyles = makeStyles((theme) => ({
+  galleryTitle: {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    padding: theme.spacing(5)
+  },
+  viewMoreButton: {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(4)
+  },
+  link: {
+    textDecoration: 'none',
+    color: theme.palette.text.primary
+  }
+}));
+
+export default function GalleryPreview(props) {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Container className={classes.galleryTitle}>
+        <Typography variant="h4" component="h2">{props.name}</Typography>
+      </Container>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        spacing={4}
+      >
+        {[0, 1, 2].map((value) => (
+          <Grid key={value} item>
+            <ImgMediaCard/>
+          </Grid>
+        ))}
+      </Grid>
+      <Container className={classes.viewMoreButton}>
+        <Link to={props.link} className={classes.link}>
+          <Button variant="contained">View More</Button>
+        </Link>
+      </Container>
+    </>
+  );
+}
