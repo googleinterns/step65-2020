627fe43eb8d69f4871cf8b9fbcadfe1068f5cf8c
Isabel Sangimino <isangimino@google.com>
2020-06-29 17:04:31 +0000

    add props to img media card

--- frontend/src/components/dashboard/ImgMediaCard.js
+++ frontend/src/components/dashboard/ImgMediaCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
+import {Link} from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,7 +8,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import PlaceholderImage from './images/placeholder-image.png';
+import PlaceholderImage from './images/paint.jpg';
 
 const useStyles = makeStyles({
   root: {
@@ -15,7 +16,7 @@ const useStyles = makeStyles({
   },
 });
 
-export default function ImgMediaCard() {
+export default function ImgMediaCard({name, link}) {
   const classes = useStyles();
 
   return (
@@ -28,15 +29,13 @@ export default function ImgMediaCard() {
           image={PlaceholderImage}
           title="Artwork"
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Artwork
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over
-            6,000 species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
+        <Link to={link}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {name}
+            </Typography>
+          </CardContent>
+        </Link>
       </CardActionArea>
       <CardActions>
         <Button size="small" color="primary">
