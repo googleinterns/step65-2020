4da6a415a402ea309125e5e7087d860bc2f79959
Isabel Sangimino <isangimino@google.com>
2020-07-01 20:47:11 +0000

    made new separate museum gallery component, simple fetch call, and display photo using iiif

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -18,13 +18,11 @@ import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import NavigationItems from './NavigationItems';
+import MuseumGallery from './MuseumGallery';
 import GalleryPreview from './GalleryPreview';
 import ArtworkCloseUpCard from './ArtworkCloseUpCard';
 import UploadsFields from './UploadsFields';
 import DescLinks from './DescLinks';
-import Gallery from './Gallery';
-import Banner from './Banner';
-import AICimg from './images/aic-inside.jpg';
 
 const drawerWidth = 240;
 
@@ -169,14 +167,7 @@ export default function Dashboard() {
             </Route>
             <Route exact path="/museum-gallery">
               <Container className={classes.galleryPageWrapper}>
-                <Banner
-                  title="Museum Gallery"
-                  description="Explore the Art Institute of Chicago!"
-                  imgURL={AICimg}
-                />
-                <Container>
-                  <Gallery />
-                </Container>
+                <MuseumGallery />
               </Container>
             </Route>
             <Route exact path="/user-uploads-gallery">
