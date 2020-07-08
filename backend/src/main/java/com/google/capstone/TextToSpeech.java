e1a4fb83811a6b2d89044c6c516c0e5dcd38985c
Indhu Gunda <indhu.gunda@utexas.edu>
2020-07-08 15:55:57 +0000

    Add tts servlet that generates audio file based on input

--- backend/src/main/java/com/google/capstone/TextToSpeech.java
+++ backend/src/main/java/com/google/capstone/TextToSpeech.java
@@ -1,9 +1,7 @@
 package com.google.capstone;
 
-import com.google.appengine.api.utils.SystemProperty;
 
 import java.io.IOException;
-import java.util.Properties;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,44 +19,46 @@ import com.google.protobuf.ByteString;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 
+
+
 @WebServlet(name = "TextToSpeech", value = "/api/v1/tts")
 public class TextToSpeech extends HttpServlet {
 
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
-    // Instantiates a client
-    try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()) {
-      // Set the text input to be synthesized
-      SynthesisInput input = SynthesisInput.newBuilder().setText("Hello, World!").build();
-
-      // Build the voice request, select the language code ("en-US") and the ssml voice gender
-      // ("neutral")
-      VoiceSelectionParams voice =
-          VoiceSelectionParams.newBuilder()
-              .setLanguageCode("en-US")
-              .setSsmlGender(SsmlVoiceGender.NEUTRAL)
-              .build();
-
-      // Select the type of audio file you want returned
-      AudioConfig audioConfig =
-          AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.MP3).build();
-
-      // Perform the text-to-speech request on the text input with the selected voice parameters and
-      // audio file type
-      SynthesizeSpeechResponse ttsResponse =
-          textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
-
-      // Get the audio contents from the response
-      ByteString audioContents = ttsResponse.getAudioContent();
-
-      // Write the response to the output file.
-      try (OutputStream out = new FileOutputStream("output.mp3")) {
-        out.write(audioContents.toByteArray());
-        System.out.println("Audio content written to file \"output.mp3\"");
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String textString = request.getParameter("text");
+    if (!textString.isEmpty()) {
+      try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()) {
+        ByteString audioContents = generateAudio(textToSpeechClient, textString);
+        createFile(audioContents, response);
+      } catch (IOException e) {
+        response.sendError(500, "Unable to generate audio file.");
       }
+    } else {
+      response.sendError(400, "Invalid text.");
     }
+  }
 
+  private ByteString generateAudio(TextToSpeechClient textToSpeechClient, String textString) {
+    SynthesisInput input = SynthesisInput.newBuilder().setText(textString).build();
+    VoiceSelectionParams voice =
+            VoiceSelectionParams.newBuilder()
+                    .setLanguageCode("en-US")
+                    .setSsmlGender(SsmlVoiceGender.NEUTRAL)
+                    .build();
+    AudioConfig audioConfig =
+            AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.MP3).build();
+    SynthesizeSpeechResponse ttsResponse =
+            textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
+    return ttsResponse.getAudioContent();
+  }
+
+  private void createFile(ByteString audioContents, HttpServletResponse response) throws IOException {
+    try (OutputStream out = new FileOutputStream("output.mp3")) {
+      out.write(audioContents.toByteArray());
+    } catch (IOException e) {
+      response.sendError(500, "Unable to create audio file.");
+    }
   }
 
 }
