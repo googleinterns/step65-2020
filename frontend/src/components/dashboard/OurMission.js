b3207f5d8d597a29643705b82ecbd9d90bb494a0
Indhu Gunda <43057952+indhu-gunda@users.noreply.github.com>
2020-07-09 13:13:15 -0500

    Redesign Mission section (#20)
    * add props to img media card
    
    * img media card is simpler and linked to closeup view
    
    * eslint changes
    
    * spacing nit
    
    * banner spacing is now correct
    
    * deleted unused variable
    
    * cropped photo
    
    * fix css styling (alphabetical order)
    
    * centered text in hero and change size of description text
    
    * Add landing page background
    
    * eslint changes
    
    * add classes to other pages to fix padding
    
    * Add header text and font
    
    * resolve merge conflict
    
    * Remove comments
    
    * bug fixes
    
    * Fix npm ci issue
    
    * Fix merge
    
    * fix merge
    
    * Redesign mission section
    
    * Fix eslint issues
    
    * Revise mission statements
    
    Co-authored-by: Isabel Sangimino <isangimino@google.com>

--- frontend/src/components/dashboard/OurMission.js
+++ frontend/src/components/dashboard/OurMission.js
@@ -3,44 +3,102 @@ import {makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import UniversalIcon from '@material-ui/icons/Public';
+import AccessibilityIcon from '@material-ui/icons/AccessibilityNew';
+import ArtIcon from '@material-ui/icons/Brush';
+import LogoImg from './images/logo.JPG';
+
 
 const useStyles = makeStyles((theme) => ({
   container: {
+    color: theme.palette.secondary.contrastText,
     padding: theme.spacing(10),
+  },
+  avatar: {
+    background: theme.palette.secondary.light,
+    height: theme.spacing(7),
+    margin: theme.spacing(2),
+    width: theme.spacing(7),
+  },
+  image: {
+    maxWidth: '13rem',
   },
 }));
 
 export default function OurMission() {
   const classes = useStyles();
+  const primaryTypographyProps = {variant: 'h6', component: 'h3'};
+
   return (
     <Container className={classes.container}>
-      <Grid container alignItems="center">
-        <Grid item xs={12} md={4}>
-          <Typography color="primary" variant="h2" alignCenter>
-            Our Mission
-          </Typography>
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justify="center"
+        spacing={4}
+      >
+        <Grid
+          container
+          item
+          xs={12}
+          md={5}
+          className={classes.title}
+          alignItems="center"
+          justify="center"
+          direction="column"
+          spacing={1}
+        >
+          <Grid item>
+            <Typography variant="h2" align="center">
+              Our Mission
+            </Typography>
+          </Grid>
+          <Grid item>
+            <img src={LogoImg} alt="logo" className={classes.image}/>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={8}>
-          <Typography variant="body1">
-            With the goal of making art universally accessible,
-            especially to those that are visually-impaired,
-            we created a website that provides a virtual
-            art experience.  This website allows artists
-            and art-lovers of all abilities to both enjoy art
-            and share their own artwork.  The website is
-            screen-reader compatible and keyboard navigable.
-            Every piece of art displayed on the website has
-            a corresponding description that expands beyond
-            a one line ALT text, and this description will
-            be read aloud when an artwork is selected.
-            While much of the artwork
-            displayed on the website is drawn from an
-            art museum’s public API, we also want to provide a
-            platform for any artist to share their art.
-            Thus, we include an accessible form for
-            users to upload and write descriptions of
-            their own artwork.
-          </Typography>
+        <Grid item xs={12} md={7}>
+          <List>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar className={classes.avatar}>
+                  <UniversalIcon fontSize="large"/>
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="Create an art experience for everyone."
+                primaryTypographyProps={primaryTypographyProps}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar className={classes.avatar}>
+                  <AccessibilityIcon fontSize="large"/>
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="Build with accessibility as the primary focus."
+                primaryTypographyProps={primaryTypographyProps}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar className={classes.avatar}>
+                  <ArtIcon fontSize="large"/>
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="Showcase the artwork of our users."
+                primaryTypographyProps={primaryTypographyProps}
+              />
+            </ListItem>
+          </List>
         </Grid>
       </Grid>
     </Container>
