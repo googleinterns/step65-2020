adf10005cf93995e2b45d8bbc87dd98d4267292d
Indhu Gunda <indhu.gunda@utexas.edu>
2020-07-15 02:19:01 +0000

    Remove comments

--- frontend/src/components/dashboard/ArtworkCloseUpCard.js
+++ frontend/src/components/dashboard/ArtworkCloseUpCard.js
@@ -55,16 +55,6 @@ export default function ArtworkCloseUpCard(props) {
     params.append('text', description);
     params.append('id', id);
     fetch('/api/v1/tts', {method: 'POST', body: params})
-        // .then((response) => response.text())
-        // .then((response) => console.log(response))
-        // .then((response) => {
-        //   const binaryData = [];
-        //   binaryData.push(response);
-        //   return binaryData;
-        // })
-        // .then((binaryData) =>
-        //   URL.createObjectURL(new Blob(binaryData, {type: "audio/mpeg"})))
-        // .then((url) => console.log(url))
         .then((response) => response.text())
         .then((blobKey) => document.getElementById('audio')
             .setAttribute('src', `/api/v1/get-blob?blob-key=${blobKey}`));
