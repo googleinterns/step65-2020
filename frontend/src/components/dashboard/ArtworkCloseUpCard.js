b133b013a77208f8549304c1b8a1522d59ce809e
Indhu Gunda <indhu.gunda@utexas.edu>
2020-07-17 05:22:16 +0000

    Fixes error on load

--- frontend/src/components/dashboard/ArtworkCloseUpCard.js
+++ frontend/src/components/dashboard/ArtworkCloseUpCard.js
@@ -39,12 +39,6 @@ const useStyles = makeStyles((theme) => ({
   withPadding: {
     padding: theme.spacing(3),
   },
-  loading: {
-    'width': '100%',
-    '& > * + *': {
-      marginTop: theme.spacing(2),
-    },
-  },
 }));
 
 export default function ArtworkCloseUpCard(props) {
@@ -52,114 +46,128 @@ export default function ArtworkCloseUpCard(props) {
   const [error, setError] = useState(false);
   const [audioLoading, setAudioLoading] = useState(true);
 
-  console.log(loading);
-
   const subheaderTypographyProps = {color: 'textSecondary'};
 
   const id = props.match.params.id;
   const artworks = useSelector((state) => (state.museumArtworks.artworks));
-  const artwork = artworks.get(id);
-  const title = artwork.get('title');
-  const artist = artwork.get('artist');
-  const alt = artwork.get('alt');
-  const description = artwork.get('description');
-  const department = artwork.get('department');
 
   useEffect(() => {
-    const descriptionElement = document.getElementById('description');
-    descriptionElement.innerHTML = description;
+    if (artworks && artworks.has(id)) {
+      const artwork = artworks.get(id);
+      const description = artwork.get('description');
+      const descriptionElement = document.getElementById('description');
+      descriptionElement.innerHTML = description;
 
-    function handleErrors(response) {
-      if (!response.ok) {
-        throw Error(response.statusText);
+      function handleErrors(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response;
       }
-      return response;
+
+      generateTextToSpeech(getAudioTranscript(artwork), id)
+          .then(handleErrors)
+          .then((response) => response.text())
+          .then((blobKey) => {
+            document.getElementById('audio')
+                .setAttribute('src', `/api/v1/get-blob?blob-key=${blobKey}`);
+          })
+          .then(() => setAudioLoading(false))
+          .catch(() => {
+            setError(true);
+            setAudioLoading(false);
+          });
     }
+  }, [artworks, id]);
 
-    generateTextToSpeech(getAudioTranscript(artwork), id)
-        .then(handleErrors)
-        .then((response) => response.text())
-        .then((blobKey) => {
-          document.getElementById('audio')
-              .setAttribute('src', `/api/v1/get-blob?blob-key=${blobKey}`);
-        })
-        .then(() => setAudioLoading(false))
-        .catch(() => {
-          setError(true);
-          setAudioLoading(false);
-        });
-  }, [artwork, id, alt, artist, department, description, title]);
+  if (!artworks || !artworks.has(id)) {
+    return (
+      <div className={classes.root}>
+        <LinearProgress color="secondary" />
+      </div>
+    );
+  } else {
+    console.log(artworks);
+    const artwork = artworks.get(id);
+    const title = artwork.get('title');
+    const artist = artwork.get('artist');
+    const alt = artwork.get('alt');
 
-  return (
-    <Container className={classes.withPadding}>
-      <Card className={classes.root}>
-        <CardHeader
-          title={title}
-          subheader={artist}
-          subheaderTypographyProps={subheaderTypographyProps}
-          className={classes.header}
-          action={
-            <>
-              <br/>
-              <AudioPlayer controls id="audio" className={classes.audioPlayer}/>
-            </>
-          }
-        />
-        {audioLoading && (
-          <div className={classes.root}>
-            <LinearProgress />
-          </div>
-        )}
-        {error && (
-          <Alert
-            severity="error"
+    return (
+      <Container className={classes.withPadding}>
+        <Card className={classes.root}>
+          <CardHeader
+            title={title}
+            subheader={artist}
+            subheaderTypographyProps={subheaderTypographyProps}
+            className={classes.header}
+            action={
+              <>
+                <br/>
+                <AudioPlayer
+                  controls
+                  id="audio"
+                  className={classes.audioPlayer}
+                />
+              </>
+            }
+          />
+          {audioLoading && (
+            <div className={classes.root}>
+              <LinearProgress />
+            </div>
+          )}
+          {error && (
+            <Alert
+              severity="error"
+            >
+                  The audio could not be loaded at this time.
+            </Alert>
+          )}
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            spacing={4}
+            className={classes.root}
           >
-            The audio could not be loaded at this time.
-          </Alert>
-        )}
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-          spacing={4}
-          className={classes.root}
-        >
-          <Grid item xs={12} md={8}>
-            <CardMedia
-              className={classes.media}
-              image={artwork.get('url')}
-              title={artist}
-            />
-            <CardContent>
-              <Typography
-                variant="body2"
-                color="primary"
-                align="center"
-                component="p"
-              >
-                {alt}
-              </Typography>
-            </CardContent>
+            <Grid item xs={12} md={8}>
+              <CardMedia
+                className={classes.media}
+                image={artwork.get('url')}
+                title={artist}
+              />
+              <CardContent>
+                <Typography
+                  variant="body2"
+                  color="primary"
+                  align="center"
+                  component="p"
+                >
+                  {alt}
+                </Typography>
+              </CardContent>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <CardContent className={classes.content}>
+                <Typography
+                  variant="h4"
+                  color="primary"
+                  component="h2"
+                  align="center"
+                  gutterBottom
+                >
+                    Description</Typography>
+                <Typography id="description" align="left" paragraph>
+                </Typography>
+              </CardContent>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={4}>
-            <CardContent className={classes.content}>
-              <Typography
-                variant="h4"
-                color="primary"
-                component="h2"
-                align="center"
-                gutterBottom
-              >
-                  Description</Typography>
-              <Typography id="description" align="left" paragraph>
-              </Typography>
-            </CardContent>
-          </Grid>
-        </Grid>
-      </Card>
-    </Container>
-  );
+        </Card>
+      </Container>
+    );
+  }
 }
 
 ArtworkCloseUpCard.propTypes = {
