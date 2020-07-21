c14c4cbb3421fbed0634ffe6b2faf654ed486ae6
katch00 <64349523+katch00@users.noreply.github.com>
2020-07-21 18:12:07 -0500

    Update ServeImage.java

--- backend/src/main/java/com/google/capstone/ServeImage.java
+++ backend/src/main/java/com/google/capstone/ServeImage.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Endpoint to get single image given id
  */
-@WebServlet("/get-image")
+@WebServlet("/getImage")
 public class ServeImage extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
