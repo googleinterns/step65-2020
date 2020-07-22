b5c7bfd281d783546b36580515ffd6e15262a62c
Indhu Gunda <43057952+indhu-gunda@users.noreply.github.com>
2020-07-22 19:56:01 +0000

    Loading Feedback (#45)
    * Create tts servlet
    
    * package-json
    
    * Add tts servlet that generates audio file based on input
    
    * Add new line
    
    * Add new line
    
    * Remove unused dependency
    
    * museum gallery displays photos and titles from API
    
    * deleting unused imports
    
    * when you hover over card, it displays artwork name
    
    * Change to POST
    
    * Setup proxy and fetch with POST request in frontend
    
    * add ternary and tightened code
    
    * Uploads to Google Cloud Storage
    
    * Use gcs url after fetch
    
    * Add comments and remove unused imports
    
    * create store
    
    * setting up reducer and actions
    
    * finish refactoring with redux
    
    * getMuseumArtworks has more broad functionality
    
    * move pagination to MuseumGallery so it can interact with redux
    
    * adds onChange function to Pagination component
    
    * passing fetchMuseumArtworks a page number parameter for api call
    
    * fix eslint
    
    * Link gallery images to unique close up view url
    
    * moved dispatch to Dashboard component, gallery preview now displays artworks from API, organized dashboard folder a little
    
    * deleted unused code
    
    * Populate close up view with information from API
    
    * Finish merge to achieve same functionality
    
    * Remove audio file
    
    * delete unused import
    
    * npx fixes
    
    * tidying the code
    
    * forgot a comma
    
    * npx fix
    
    * Add artist
    
    * Minor fixes and remove console.log()s
    
    * Remove console.log
    
    * Use BlobstoreService to serve the audio files from Google Cloud Storage.
    
    * Remove comments
    
    * Remove unused imports and variables
    
    * query api for artworks with public domain and descriptions
    
    * fetch additional fields
    
    * Strip html tags from description before requesting tts file
    
    * delete unneeded check
    
    * fix merge conflict
    
    * Creates a more tour like audio
    
    * UI changes
    
    * remove mp3 file (#35)
    
    * Add error alert and logging
    
    * Make audio load faster the second time
    
    * Generate audio files on gallery view so loading time less
    
    * fix undefined
    
    * Add loading bar.
    
    * wip
    
    * Add loading bar for audio
    
    * Fixes error on load
    
    * Remove log
    
    Co-authored-by: indhu.gunda@utexas.edu <igunda@google.com>
    Co-authored-by: Isabel Sangimino <isangimino@google.com>

--- frontend/src/components/dashboard/ArtworkCloseUpCard.js
+++ frontend/src/components/dashboard/ArtworkCloseUpCard.js
@@ -9,6 +9,7 @@ import Grid from '@material-ui/core/Grid';
 import Alert from '@material-ui/lab/Alert';
 import AudioPlayer from 'react-audio-player';
 import Container from '@material-ui/core/Container';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import PropTypes from 'prop-types';
 import {useSelector} from 'react-redux';
 import {generateTextToSpeech, getAudioTranscript} from './textToSpeechHelpers';
@@ -43,102 +44,129 @@ const useStyles = makeStyles((theme) => ({
 export default function ArtworkCloseUpCard(props) {
   const classes = useStyles();
   const [error, setError] = useState(false);
+  const [audioLoading, setAudioLoading] = useState(true);
 
   const subheaderTypographyProps = {color: 'textSecondary'};
 
   const id = props.match.params.id;
   const artworks = useSelector((state) => (state.museumArtworks.artworks));
-  const artwork = artworks.get(id);
-  const title = artwork.get('title');
-  const artist = artwork.get('artist');
-  const alt = artwork.get('alt');
-  const description = artwork.get('description');
-  const department = artwork.get('department');
 
   useEffect(() => {
-    const descriptionElement = document.getElementById('description');
-    descriptionElement.innerHTML = description;
+    if (artworks && artworks.has(id)) {
+      const artwork = artworks.get(id);
+      const description = artwork.get('description');
+      const descriptionElement = document.getElementById('description');
+      descriptionElement.innerHTML = description;
 
-    function handleErrors(response) {
-      if (!response.ok) {
-        throw Error(response.statusText);
+      function handleErrors(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response;
       }
-      return response;
+
+      generateTextToSpeech(getAudioTranscript(artwork), id)
+          .then(handleErrors)
+          .then((response) => response.text())
+          .then((blobKey) => {
+            document.getElementById('audio')
+                .setAttribute('src', `/api/v1/get-blob?blob-key=${blobKey}`);
+          })
+          .then(() => setAudioLoading(false))
+          .catch(() => {
+            setError(true);
+            setAudioLoading(false);
+          });
     }
+  }, [artworks, id]);
 
-    generateTextToSpeech(getAudioTranscript(artwork), id)
-        .then(handleErrors)
-        .then((response) => response.text())
-        .then((blobKey) => document.getElementById('audio')
-            .setAttribute('src', `/api/v1/get-blob?blob-key=${blobKey}`))
-        .catch(() => setError(true));
-  }, [artwork, id, alt, artist, department, description, title]);
+  if (!artworks || !artworks.has(id)) {
+    return (
+      <div className={classes.root}>
+        <LinearProgress color="secondary" />
+      </div>
+    );
+  } else {
+    const artwork = artworks.get(id);
+    const title = artwork.get('title');
+    const artist = artwork.get('artist');
+    const alt = artwork.get('alt');
 
-  return (
-    <Container className={classes.withPadding}>
-      <Card className={classes.root}>
-        <CardHeader
-          title={title}
-          subheader={artist}
-          subheaderTypographyProps={subheaderTypographyProps}
-          className={classes.header}
-          action={
-            <>
-              <br/>
-              <AudioPlayer controls id="audio" className={classes.audioPlayer}/>
-            </>
-          }
-        />
-        {error && (
-          <Alert
-            severity="error"
+    return (
+      <Container className={classes.withPadding}>
+        <Card className={classes.root}>
+          <CardHeader
+            title={title}
+            subheader={artist}
+            subheaderTypographyProps={subheaderTypographyProps}
+            className={classes.header}
+            action={
+              <>
+                <br/>
+                <AudioPlayer
+                  controls
+                  id="audio"
+                  className={classes.audioPlayer}
+                />
+              </>
+            }
+          />
+          {audioLoading && (
+            <div className={classes.root}>
+              <LinearProgress />
+            </div>
+          )}
+          {error && (
+            <Alert
+              severity="error"
+            >
+                  The audio could not be loaded at this time.
+            </Alert>
+          )}
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            spacing={4}
+            className={classes.root}
           >
-            The audio could not be loaded at this time.
-          </Alert>
-        )}
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-          spacing={4}
-          className={classes.root}
-        >
-          <Grid item xs={12} md={8}>
-            <CardMedia
-              className={classes.media}
-              image={artwork.get('url')}
-              title={artist}
-            />
-            <CardContent>
-              <Typography
-                variant="body2"
-                color="primary"
-                align="center"
-                component="p"
-              >
-                {alt}
-              </Typography>
-            </CardContent>
+            <Grid item xs={12} md={8}>
+              <CardMedia
+                className={classes.media}
+                image={artwork.get('url')}
+                title={artist}
+              />
+              <CardContent>
+                <Typography
+                  variant="body2"
+                  color="primary"
+                  align="center"
+                  component="p"
+                >
+                  {alt}
+                </Typography>
+              </CardContent>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <CardContent className={classes.content}>
+                <Typography
+                  variant="h4"
+                  color="primary"
+                  component="h2"
+                  align="center"
+                  gutterBottom
+                >
+                    Description</Typography>
+                <Typography id="description" align="left" paragraph>
+                </Typography>
+              </CardContent>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={4}>
-            <CardContent className={classes.content}>
-              <Typography
-                variant="h4"
-                color="primary"
-                component="h2"
-                align="center"
-                gutterBottom
-              >
-                  Description</Typography>
-              <Typography id="description" align="left" paragraph>
-              </Typography>
-            </CardContent>
-          </Grid>
-        </Grid>
-      </Card>
-    </Container>
-  );
+        </Card>
+      </Container>
+    );
+  }
 }
 
 ArtworkCloseUpCard.propTypes = {
