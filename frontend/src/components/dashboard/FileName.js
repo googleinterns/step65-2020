b2c45ec0dc123ea1241efa7705b0a0dc1749aa60
Indhu Gunda <indhu.gunda@utexas.edu>
2020-07-22 19:46:39 +0000

    eslint issues

--- frontend/src/components/dashboard/FileName.js
+++ frontend/src/components/dashboard/FileName.js
@@ -4,7 +4,6 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import PhotoUploadIcon from '@material-ui/icons/AddPhotoAlternate';
-import PropTypes from 'prop-types';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,20 +21,18 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function FileName(props) {
-
   const onChange = (e) => {
     switch (e.target.name) {
       case 'selectedFile':
-        if(e.target.files.length > 0) {
-          // Accessed .name from file 
+        if (e.target.files.length > 0) {
+          // Accessed .name from file
           setName(e.target.files[0].name);
         }
         break;
       default:
 
         setName( e.target.value);
-     }
-
+    }
   };
 
   useEffect(() => {
@@ -47,7 +44,7 @@ export default function FileName(props) {
           console.log(imageUploadUrl);
           setURL(imageUploadUrl);
         });
-  },[]);
+  }, []);
 
   const classes = useStyles();
 
@@ -56,55 +53,60 @@ export default function FileName(props) {
 
   const [actionURL, setURL] = useState('');
 
-  file = fileName 
-      ? ( <span>File Selected - {fileName}</span>) 
-      : ( <span>Choose a file...</span> );
+  file = fileName ?
+      ( <span>File Selected - {fileName}</span>) :
+      ( <span>Choose a file...</span> );
 
-  return(
-      <>
-        <form name="image-upload" action={actionURL} method="POST" encType="multipart/form-data">
-          <Grid 
-            alignItems="center"
-            container
-            className={classes.root}
-            justify="flex-start"
-            spacing={2}
-          >
-            <Grid item xs={8}>
-              <Box
-                display="flex"
-                flexDirection="row"
-                p={1}
-                m={1}
-                bgcolor="grey.300"
-              >
-                {file}
-              </Box>
-            </Grid>
-            <Grid item>
-              <input
-                accept="image/*"
-                className={classes.input}
-                id="select-file"
-                type="file"
-                name="selectedFile"
-                onChange={onChange}
-              />
-              <label htmlFor="select-file">
-                <Button
-                  variant="contained"
-                  color="primary"
-                  component="span"
-                  id="shown-button"
-                  startIcon={<PhotoUploadIcon />}
-                >
-                  Select File
-                </Button>
-              </label>
-              <input type="submit" />
-            </Grid>
+  return (
+    <>
+      <form
+        name="image-upload"
+        action={actionURL}
+        method="POST"
+        encType="multipart/form-data"
+      >
+        <Grid
+          alignItems="center"
+          container
+          className={classes.root}
+          justify="flex-start"
+          spacing={2}
+        >
+          <Grid item xs={8}>
+            <Box
+              display="flex"
+              flexDirection="row"
+              p={1}
+              m={1}
+              bgcolor="grey.300"
+            >
+              {file}
+            </Box>
           </Grid>
-        </form>
-      </>
+          <Grid item>
+            <input
+              accept="image/*"
+              className={classes.input}
+              id="select-file"
+              type="file"
+              name="selectedFile"
+              onChange={onChange}
+            />
+            <label htmlFor="select-file">
+              <Button
+                variant="contained"
+                color="primary"
+                component="span"
+                id="shown-button"
+                startIcon={<PhotoUploadIcon />}
+              >
+                  Select File
+              </Button>
+            </label>
+            <input type="submit" />
+          </Grid>
+        </Grid>
+      </form>
+    </>
   );
 }
