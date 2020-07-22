c4ce41bea7a47a30a8b4ff196c40aa96bcf21b6b
katch00 <64349523+katch00@users.noreply.github.com>
2020-07-22 13:33:10 -0500

    Class to function (#42)
    * set up front/backend, allow user to upload info to datastore
    
    * add new lines
    
    * fix style  mistakes
    
    * add comments to java files
    
    * small eslint/spelling fixes
    
    * fix package-lock
    
    * fixes
    
    * add servlet and connection to allow user to upload imgs.
    
    * fix imports
    
    * forgot to add the sumbit fix
    
    * fix sumbit button repeat and indents
    
    * add redirect url for dev and deployed
    
    * fix spacing
    
    * change fileName to a function rather than a class
    
    * fix indenting from changes, delete comment
    
    * fix indenting from changes

--- frontend/src/components/dashboard/FileName.js
+++ frontend/src/components/dashboard/FileName.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {withStyles} from '@material-ui/core/styles';
+import React, {useState, useEffect} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import PhotoUploadIcon from '@material-ui/icons/AddPhotoAlternate';
 import PropTypes from 'prop-types';
 
-const useStyles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
       marginLeft: 0,
@@ -19,69 +19,50 @@ const useStyles = (theme) => ({
   input: {
     display: 'none',
   },
-});
+}));
 
-class FileName extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fileName: '',
-      actionURL: '',
-    };
-    this.getBlobstoreURL();
-  }
+export default function FileName(props) {
 
-  onChange = (e) => {
+  const onChange = (e) => {
     switch (e.target.name) {
       case 'selectedFile':
-        if (e.target.files.length > 0) {
-          // Accessed .name from file
-          this.setState({fileName: e.target.files[0].name});
+        if(e.target.files.length > 0) {
+          // Accessed .name from file 
+          setName(e.target.files[0].name);
         }
         break;
       default:
-        this.setState({[e.target.name]: e.target.value});
-    }
-  }
 
-  getBlobstoreURL = () => {
+        setName( e.target.value);
+     }
+
+  };
+
+  useEffect(() => {
     fetch('/api/v1/uploadImgs')
         .then((response) => {
           return response.text();
         })
         .then((imageUploadUrl) => {
           console.log(imageUploadUrl);
-          this.setState({actionURL: imageUploadUrl});
+          setURL(imageUploadUrl);
         });
-  };
+  },[]);
 
-  getBlobstoreURL = () => {
-    fetch('/api/v1/uploadImgs')
-        .then((response) => {
-          return response.text();
-        })
-        .then((imageUploadUrl) => {
-          console.log(imageUploadUrl);
-          this.setState({actionURL: imageUploadUrl});
-        });
-  };
+  const classes = useStyles();
 
-  render(){
-    const {classes} = this.props;
+  const [fileName, setName] = useState('');
+  let file = null;
 
-    const {fileName} = this.state;
-    let file = null;
+  const [actionURL, setURL] = useState('');
 
-    const {actionURL} = this.state;
-
-    file = fileName 
+  file = fileName 
       ? ( <span>File Selected - {fileName}</span>) 
       : ( <span>Choose a file...</span> );
 
-    return (
+  return(
       <>
-      <body>
-        <form name="image-upload" action={actionURL} method="POST" enctype="multipart/form-data">
+        <form name="image-upload" action={actionURL} method="POST" encType="multipart/form-data">
           <Grid 
             alignItems="center"
             container
@@ -107,7 +88,7 @@ class FileName extends React.Component {
                 id="select-file"
                 type="file"
                 name="selectedFile"
-                onChange={ (event) => this.onChange(event) }
+                onChange={onChange}
               />
               <label htmlFor="select-file">
                 <Button
@@ -124,14 +105,6 @@ class FileName extends React.Component {
             </Grid>
           </Grid>
         </form>
-      </body>
       </>
-    );
-  }
+  );
 }
-
-FileName.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(useStyles)(FileName);
