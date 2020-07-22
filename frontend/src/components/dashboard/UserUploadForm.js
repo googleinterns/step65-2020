536cef781fe1f4b7a41a8b6d73fd4a0391874a62
isabelsang <42744910+isabelsang@users.noreply.github.com>
2020-07-22 18:15:49 -0500

    Submit search by hitting "enter" key and "no results found" edge case (#49)
    * package-json
    
    * museum gallery displays photos and titles from API
    
    * deleting unused imports
    
    * when you hover over card, it displays artwork name
    
    * add ternary and tightened code
    
    * create store
    
    * setting up reducer and actions
    
    * finish refactoring with redux
    
    * getMuseumArtworks has more broad functionality
    
    * move pagination to MuseumGallery so it can interact with redux
    
    * adds onChange function to Pagination component
    
    * passing fetchMuseumArtworks a page number parameter for api call
    
    * fix eslint
    
    * moved dispatch to Dashboard component, gallery preview now displays artworks from API, organized dashboard folder a little
    
    * deleted unused code
    
    * delete unused import
    
    * npx fixes
    
    * tidying the code
    
    * forgot a comma
    
    * npx fix
    
    * query api for artworks with public domain and descriptions
    
    * fetch additional fields
    
    * delete unneeded check
    
    * fix merge conflict
    
    * title truncated
    
    * remove form and get search input
    
    * merge with master
    
    * move banner into and pagination out of dashboard
    
    * search bar is functional
    
    * npx fixes
    
    * add constants for Dashboard
    
    * ran npm install
    
    * search bar now works using enter key
    
    * edge case for no results from search query
    
    * fix logic
    
    * change to strict equality operator
    
    * npx fixes
    
    * when user makes a new search, museumPage is set back to 1
    
    * npx fixes
    
    Co-authored-by: Isabel Sangimino <isangimino@google.com>
    Co-authored-by: Indhu Gunda <43057952+indhu-gunda@users.noreply.github.com>

--- frontend/src/components/dashboard/UserUploadForm.js
+++ frontend/src/components/dashboard/UserUploadForm.js
@@ -20,22 +20,21 @@ const useStyles = makeStyles((theme) => ({
   file: {
     height: '40px',
     width: '75ch',
-  }
+  },
 }));
 
 export default function UserUploadForm(props) {
-
   const onChange = (e) => {
     switch (e.target.name) {
       case 'selectedFile':
-        if(e.target.files.length > 0) {
-          // Accessed .name from file 
+        if (e.target.files.length > 0) {
+          // Accessed .name from file
           setName(e.target.files[0].name);
         }
-      break;
+        break;
       default:
         setName( e.target.value);
-     }
+    }
   };
 
   useEffect(() => {
@@ -47,7 +46,7 @@ export default function UserUploadForm(props) {
           console.log(imageUploadUrl);
           setURL(imageUploadUrl);
         });
-  },[]);
+  }, []);
 
   const classes = useStyles();
 
@@ -56,110 +55,115 @@ export default function UserUploadForm(props) {
 
   const [actionURL, setURL] = useState('');
 
-  file = fileName 
-      ? ( <span>File Selected - {fileName}</span>) 
-      : ( <span>Choose a file...</span> );
+  file = fileName ?
+      ( <span>File Selected - {fileName}</span>) :
+      ( <span>Choose a file...</span> );
 
-  return(
-      <>
-        <form name="image-upload" action={actionURL} method="POST" encType="multipart/form-data">
-          <Grid
-            className={classes.root}
-            container
-            direction="row"
-            justify="flex-start"
-            alignItems="center"
-          >
-            <div>
-              <TextField
-                required
-                id="artist-name"
-                label="Artist Name"
-                name="artistName"
-                variant="outlined"
-              />
-            </div>
-            <div>
-              <TextField
-                required
-                id="art-title"
-                label="Art Title"
-                name="artTitle"
-                variant="outlined"
-              />
-            </div>
-            <div>
-              <TextField
-                required
-                id="image-info"
-                label="Image Description"
-                multiline
-                name="description"
-                placeholder="In this image..."
-                rows = {4}
-                variant="outlined"
-              />
-            </div>
+  return (
+    <>
+      <form
+        name="image-upload"
+        action={actionURL}
+        method="POST"
+        encType="multipart/form-data"
+      >
+        <Grid
+          className={classes.root}
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="center"
+        >
+          <div>
+            <TextField
+              required
+              id="artist-name"
+              label="Artist Name"
+              name="artistName"
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              required
+              id="art-title"
+              label="Art Title"
+              name="artTitle"
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              required
+              id="image-info"
+              label="Image Description"
+              multiline
+              name="description"
+              placeholder="In this image..."
+              rows = {4}
+              variant="outlined"
+            />
+          </div>
+        </Grid>
+        <Grid
+          alignItems="center"
+          container
+          className={classes.root}
+          justify="center"
+          spacing={2}
+        >
+          <Grid item xs={8}>
+            <Box
+              display="flex"
+              flexDirection="row"
+              p={1}
+              m={1}
+              bgcolor="grey.300"
+            >
+              {file}
+            </Box>
           </Grid>
-          <Grid 
-            alignItems="center"
-            container 
-            className={classes.root}
-            justify="center" 
-            spacing={2}
-          >
-            <Grid item xs={8}>
-              <Box 
-                display="flex"
-                flexDirection="row"
-                p={1}
-                m={1}
-                bgcolor="grey.300"
-              >
-                {file}
-              </Box>
-            </Grid>
-            <Grid item>
-              <input
-                accept="image/*"
-                className={classes.input}
-                id="select-file"
-                type="file"
-                name="selectedFile"
-                onChange={onChange}
-              />
-              <label htmlFor="select-file">
-                <Button 
-                  variant="contained" 
-                  color="primary" 
-                  component="span"
-                  id="shown-button"
-                  startIcon={<PhotoUploadIcon />}
-                >
-                  Select File
-                </Button>
-              </label>
-            </Grid>
-          </Grid>
-          <Box m={4} />
-          <Grid
-            container
-            direction="row"
-            justify="flex-end"
-            alignItems="center"
-            className={classes.root}
-          >
-            <div>
+          <Grid item>
+            <input
+              accept="image/*"
+              className={classes.input}
+              id="select-file"
+              type="file"
+              name="selectedFile"
+              onChange={onChange}
+            />
+            <label htmlFor="select-file">
               <Button
                 variant="contained"
-                color="secondary"
-                type="submit"
+                color="primary"
+                component="span"
+                id="shown-button"
+                startIcon={<PhotoUploadIcon />}
               >
-                Submit Artwork
+                  Select File
               </Button>
-            </div>
+            </label>
           </Grid>
-        </form>
-      </>
+        </Grid>
+        <Box m={4} />
+        <Grid
+          container
+          direction="row"
+          justify="flex-end"
+          alignItems="center"
+          className={classes.root}
+        >
+          <div>
+            <Button
+              variant="contained"
+              color="secondary"
+              type="submit"
+            >
+                Submit Artwork
+            </Button>
+          </div>
+        </Grid>
+      </form>
+    </>
   );
 }
