ac972c6da3860c572335b454badfe068ffd1b268
Nat <nstroupe0215@gmail.com>
2020-07-23 23:48:08 +0000

    add loading

--- frontend/src/components/dashboard/UploadsGallery.js
+++ frontend/src/components/dashboard/UploadsGallery.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Gallery from './gallery-components/Gallery';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import Pagination from '@material-ui/lab/Pagination';
 import {fetchUserArtworks} from '../../redux/userArtworkActions';
 
@@ -10,6 +11,13 @@ const useStyles = makeStyles((theme) => ({
   pagination: {
     display: 'flex',
     justifyContent: 'center',
+    marginTop: theme.spacing(2),
+  },
+  loading: {
+    flexGrow: 1,
+    flexDirection: 'row',
+  },
+  gallery: {
     marginTop: theme.spacing(2),
   },
 }));
@@ -20,8 +28,10 @@ export default function UploadsGallery() {
   const LIMIT = 9;
 
   const [uploadsPageNum, setUploadsPageNum] = useState(1);
-
   const [pageNums, setPageNums] = useState(1);
+  const artworksLoading = useSelector(
+      (state) => (state.userArtworks.loading));
+
   const handleChangePage = (event, value) => {
     setUploadsPageNum(value);
   };
@@ -40,17 +50,24 @@ export default function UploadsGallery() {
 
   return (
     <>
-      <Container>
+      {artworksLoading && (
+        <div className={classes.root}>
+          <LinearProgress />
+        </div>
+      )}
+      <Container className={classes.gallery}>
         <Gallery isMuseum={false}/>
       </Container>
-      <Container className={classes.pagination}>
-        <Pagination
-          count={pageNums}
-          size="large"
-          page={uploadsPageNum}
-          onChange={handleChangePage}
-        />
-      </Container>
+      {!artworksLoading && (
+        <Container className={classes.pagination}>
+          <Pagination
+            count={pageNums}
+            size="large"
+            page={uploadsPageNum}
+            onChange={handleChangePage}
+          />
+        </Container>
+      )}
     </>
   );
 }
