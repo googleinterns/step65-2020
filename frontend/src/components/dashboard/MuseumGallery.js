99b537ae4e01dc01f306a46cfa1963b277ab4e52
Isabel Sangimino <isangimino@google.com>
2020-07-27 23:08:45 +0000

    add filter bar

--- frontend/src/components/dashboard/MuseumGallery.js
+++ frontend/src/components/dashboard/MuseumGallery.js
@@ -5,6 +5,7 @@ import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
+import Menu from '@material-ui/core/Menu';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import {makeStyles} from '@material-ui/core/styles';
@@ -15,7 +16,7 @@ import Typography from '@material-ui/core/Typography';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import InputBase from '@material-ui/core/InputBase';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   loading: {
@@ -44,7 +45,7 @@ const useStyles = makeStyles((theme) => ({
   searchTextField: {
     border: 'solid 1px #c3c3c3',
     borderRadius: '15px',
-    margin: theme.spacing(2),
+    marginTop: theme.spacing(2),
     padding: theme.spacing(2),
     width: '60ch',
   },
@@ -54,14 +55,24 @@ const useStyles = makeStyles((theme) => ({
   },
   filterDrawer: {
     display: 'none',
-    justifyContent: 'center',
-    paddingBottom: theme.spacing(5),
+    paddingBottom: theme.spacing(2),
   },
-  drawerButton: {
-    width: 'auto',
+  filters: {
+    'backgroundColor': theme.palette.primary,
+    'border': 'solid 1px #c3c3c3',
+    'borderRadius': '15px',
+    'display': 'inline-block',
+    'padding': theme.spacing(1),
+    'margin': theme.spacing(1),
+    'width': 'auto',
+    '&:hover': {border: 'solid 1px black'},
   },
-  filterSelects: {
+  sortByMenu: {
+    display: 'none',
     margin: theme.spacing(2),
+  },
+  surpriseMe: {
+    padding: theme.spacing(2)*.75,
   },
   pagination: {
     display: 'flex',
@@ -73,15 +84,6 @@ const useStyles = makeStyles((theme) => ({
 export default function MuseumGallery() {
   const classes = useStyles();
 
-  const handleDrawerChange = (event) => {
-    const content = document.getElementById('filter-drawer');
-    if (content.style.display === 'flex') {
-      content.style.display = 'none';
-    } else {
-      content.style.display = 'flex';
-    }
-  };
-
   /*
    only set newQuery to be true when there is a change to the page number,
    the "sort by" field, or when the search button is pressed
@@ -89,9 +91,10 @@ export default function MuseumGallery() {
   const [newQuery, setNewQuery] = React.useState(false);
 
   const [sortBy, setSortBy] = React.useState('relevance');
-  const handleChangeSortBy = (event) => {
+  const handleChangeSortBy = (event, value) => {
     setNewQuery(true);
-    setSortBy(event.target.value);
+    setSortBy(value);
+    handleCloseSortByMenu();
   };
   const [museumPage, setMuseumPage] = useState(1);
   const handleChangePage = (event, value) => {
@@ -103,10 +106,25 @@ export default function MuseumGallery() {
     setNewQuery(true);
     setSearchQuery(document.getElementById('search-textfield').value);
     setMuseumPage(1);
+    document.getElementById('filter-drawer').style.display = 'block';
   };
+  const clearSearchQuery = () => {
+    document.getElementById('filter-drawer').style.display = 'none';
+    setNewQuery(true);
+    setSearchQuery('');
+    setMuseumPage(1);
+  };
+
   const [searchField, setSearchField] = React.useState('all-fields');
   const handleChangeSearchField = (event) => {
     setSearchField(event.target.value);
+  };
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClickSortByMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseSortByMenu = () => {
+    setAnchorEl(null);
   };
 
   const artworksMap = useSelector(
@@ -154,20 +172,20 @@ export default function MuseumGallery() {
         </div>
       )}
       <Container className={classes.searchAndSortByBar}>
-        {/*<Container className={classes.searchByContainer}>*/}
-        {/*  <FormControl variant="outlined" className={classes.formControl}>*/}
-        {/*    <Select*/}
-        {/*      id="search-field"*/}
-        {/*      value={searchField}*/}
-        {/*      onChange={handleChangeSearchField}*/}
-        {/*    >*/}
-        {/*      <MenuItem value="all-fields">Search by: All fields</MenuItem>*/}
-        {/*      <MenuItem value="artist_title">Artist</MenuItem>*/}
-        {/*      <MenuItem value="description">Description</MenuItem>*/}
-        {/*      <MenuItem value="title">Title</MenuItem>*/}
-        {/*    </Select>*/}
-        {/*  </FormControl>*/}
-        {/*</Container>*/}
+        <Container className={classes.searchByContainer}>
+          <FormControl variant="outlined" className={classes.formControl}>
+            <Select
+              id="search-field"
+              value={searchField}
+              onChange={handleChangeSearchField}
+            >
+              <MenuItem value="all-fields">Search by: All fields</MenuItem>
+              <MenuItem value="artist_title">Artist</MenuItem>
+              <MenuItem value="description">Description</MenuItem>
+              <MenuItem value="title">Title</MenuItem>
+            </Select>
+          </FormControl>
+        </Container>
         <Container className={classes.searchComponents}>
           <InputBase
             placeholder="Search…"
@@ -189,52 +207,61 @@ export default function MuseumGallery() {
             </IconButton>
           </div>
         </Container>
-        <Container className={classes.drawerButton}>
-          <IconButton
-            aria-label="filtering options"
-            onClick={handleDrawerChange}
-          >
-            <Typography>Filtering</Typography>
-            <ExpandMoreIcon/>
-          </IconButton>
-        </Container>
-        <Button variant="contained" color="secondary">
-          Random artwork
+        <Button
+          variant="contained"
+          color="secondary"
+          className={classes.surpriseMe}
+        >
+          Surprise me!
         </Button>
       </Container>
       <Container id="filter-drawer" className={classes.filterDrawer}>
-          {/*<FormControl variant="outlined" className={classes.formControl}>*/}
-          {/*  <Select*/}
-          {/*    id="search-field"*/}
-          {/*    value={searchField}*/}
-          {/*    onChange={handleChangeSearchField}*/}
-          {/*  >*/}
-          {/*    <MenuItem value="all-fields">Search by: All fields</MenuItem>*/}
-          {/*    <MenuItem value="artist_title">Artist</MenuItem>*/}
-          {/*    <MenuItem value="description">Description</MenuItem>*/}
-          {/*    <MenuItem value="title">Title</MenuItem>*/}
-          {/*  </Select>*/}
-          {/*</FormControl>*/}
-        <FormControl
-          variant="filled"
-          className={[classes.filterSelects, classes.formControl].join(' ')}
+        <Button
+          aria-controls="sort-by-menu"
+          aria-haspopup="true"
+          className={classes.filters}
+          onClick={handleClickSortByMenu}
         >
-          <InputLabel shrink id="placeholder-label">Search By</InputLabel>
-          <Select
-            labelId="placeholder-label"
-            id="placeholder"
-            label="Placeholder"
-            value="allFields"
+          Sort By: {sortBy} &#x25BC;
+        </Button>
+        <Menu
+          id="sort-by-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleCloseSortByMenu}
+        >
+          <MenuItem
+            onClick={(event) => handleChangeSortBy(event, 'relevance')}
           >
-            <MenuItem value="allFields">All fields</MenuItem>
-            <MenuItem value="artist">Placeholder</MenuItem>
-            <MenuItem value="date">Placeholder</MenuItem>
-            <MenuItem value="title">Placeholder</MenuItem>
-          </Select>
-        </FormControl>
+            Relevance
+          </MenuItem>
+          <MenuItem
+            onClick={(event) => handleChangeSortBy(event, 'artist')}
+          >
+            Artist
+          </MenuItem>
+          <MenuItem
+            onClick={(event) => handleChangeSortBy(event, 'date')}
+          >
+            Date
+          </MenuItem>
+          <MenuItem
+            onClick={(event) => handleChangeSortBy(event, 'title')}
+          >
+            Title
+          </MenuItem>
+        </Menu>
+        <Button
+          id="search-filter"
+          className={classes.filters}
+          onClick={clearSearchQuery}
+        >
+          Search by: {searchQuery} &#10006;
+        </Button>
         <FormControl
           variant="filled"
-          className={[classes.filterSelects, classes.formControl].join(' ')}
+          className={[classes.sortByMenu, classes.formControl].join(' ')}
         >
           <InputLabel shrink id="sort-by-label">Sort By</InputLabel>
           <Select
