a6796732e06e8019073af061d178d66a8c050513
Indhu Gunda <indhu.gunda@utexas.edu>
2020-07-31 14:02:05 +0000

    favorites pagination

---
+++ backend/src/main/java/com/google/capstone/FindFavorite.java
@@ -0,0 +1,54 @@
+package com.google.capstone;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.gson.Gson;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+/** Servlet responsible for counting number of favorites for given user */
+@WebServlet(name = "findFavorite", value = "/api/v1/find-favorite")
+public class FindFavorite extends HttpServlet {
+
+  private static final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+  private static final Gson gson = new Gson();
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String uid = request.getParameter("uid");
+    String artworkId = request.getParameter("artwork-id");
+    String collection = request.getParameter("collection");
+
+    response.setContentType("application/json");
+    response.getWriter().println(gson.toJson(findFavorite(uid, artworkId, collection)));
+  }
+
+  private Favorite findFavorite(String uid, String artworkId, String collection) {
+    Filter artworkIdFilter = new FilterPredicate("artworkId", FilterOperator.EQUAL, artworkId);
+    Filter collectionFilter = new FilterPredicate("collection", FilterOperator.EQUAL, collection);
+    Filter artworkIdAndCollectionFilter = CompositeFilterOperator.and(artworkIdFilter, collectionFilter);
+
+    Query query = new Query(uid + "Favorites").setFilter(artworkIdAndCollectionFilter);
+    List<Entity> results = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(1));
+
+    List<Favorite> favorites = results.stream()
+        .map(entity -> Favorite.entityToFavoriteConverter(entity))
+        .collect(Collectors.toList());
+    return favorites.isEmpty() ? null : favorites.get(0);
+  }
+}
