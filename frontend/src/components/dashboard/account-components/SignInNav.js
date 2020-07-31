fabdf13f9389514d1708372358ad5a2f871860b5
katch00 <64349523+katch00@users.noreply.github.com>
2020-07-31 17:11:30 -0500

    Uploads wrap (#72)
    * Add sign-in using firebase authentication
    
    * new line
    
    * eslint fixes
    
    * eslint fixes
    
    * UI for favorite
    
    * Add private route
    
    * store user UID and require sign in to upload
    
    * change parameter stuff
    
    * delete modules I hope
    
    * fix merge mistake
    
    * add my art set up, change drawer a bit (#74)
    
    * add my art set up, change drawer a bit
    
    * Update SignInNav.js
    
    * Update SignedInNavItems.js
    
    * My art2 (#76)
    
    * get user's uploads on My Art Page
    
    * fix copy/paste mistake
    
    * Update Banner.js
    
    * Update MyArtworksGallery.js
    
    * fix merge problems
    
    Co-authored-by: Indhu Gunda <indhu.gunda@utexas.edu>

---
+++ frontend/src/components/dashboard/account-components/SignInNav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+
+import {Link} from 'react-router-dom';
+
+import {
+  List, ListItem,
+  ListItemIcon, ListItemText,
+} from '@material-ui/core';
+
+import SignInIcon from '@material-ui/icons/AccountCircle';
+
+const useStyles = makeStyles((theme) => ({
+  link: {
+    color: theme.palette.text.primary,
+    textDecoration: 'none',
+  },
+}));
+
+export default function SignInNav() {
+  const classes = useStyles();
+  return (
+    <List>
+      <Link to="/sign-in" className={classes.link}>
+        <ListItem button>
+          <ListItemIcon>
+            <SignInIcon />
+          </ListItemIcon>
+          <ListItemText primary={'Sign In'} />
+        </ListItem>
+      </Link>
+    </List>
+  );
+}
--- frontend/src/redux/rootReducer.js
+++ frontend/src/redux/rootReducer.js
@@ -1,12 +1,14 @@
 import {combineReducers} from 'redux';
 import museumArtworks from './museumArtworkReducer';
 import userArtworks from './userArtworkReducer';
+import myArtworks from './myArtworksReducer';
 import favorites from './favorites/favoritesReducer';
 import {firebaseReducer} from 'react-redux-firebase';
 
 export default combineReducers({
   museumArtworks,
   userArtworks,
+  myArtworks,
   favorites,
   firebase: firebaseReducer,
 });
