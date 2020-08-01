de1570d2a01f6350d8d1cca71176da21c26b56ec
Isabel Sangimino <42744910+isabelsang@users.noreply.github.com>
2020-07-31 20:21:18 -0400

    Fixed bug with "surprise me" button (#87)
    * package-json
    
    * museum gallery displays photos and titles from API
    
    * deleting unused imports
    
    * when you hover over card, it displays artwork name
    
    * add ternary and tightened code
    
    * create store
    
    * setting up reducer and actions
    
    * finish refactoring with redux
    
    * getMuseumArtworks has more broad functionality
    
    * move pagination to MuseumGallery so it can interact with redux
    
    * adds onChange function to Pagination component
    
    * passing fetchMuseumArtworks a page number parameter for api call
    
    * fix eslint
    
    * moved dispatch to Dashboard component, gallery preview now displays artworks from API, organized dashboard folder a little
    
    * deleted unused code
    
    * delete unused import
    
    * npx fixes
    
    * tidying the code
    
    * forgot a comma
    
    * npx fix
    
    * query api for artworks with public domain and descriptions
    
    * fetch additional fields
    
    * delete unneeded check
    
    * fix merge conflict
    
    * title truncated
    
    * remove form and get search input
    
    * merge with master
    
    * move banner into and pagination out of dashboard
    
    * search bar is functional
    
    * npx fixes
    
    * add constants for Dashboard
    
    * ran npm install
    
    * search bar now works using enter key
    
    * edge case for no results from search query
    
    * fix logic
    
    * change to strict equality operator
    
    * npx fixes
    
    * when user makes a new search, museumPage is set back to 1
    
    * sort by date
    
    * mapping
    
    * not working
    
    * midst of experimenting
    
    * filter by select menu works
    
    * delete comments and fix spacing
    
    * search by in progress
    
    * add default value for sort by parameter in fetch
    
    * working with hard coded values
    
    * implementing search by
    
    * search by feature works
    
    * fix spacing and removed unused component
    
    * move search by menu
    
    * npx fixes
    
    * npx fixes
    
    * loading bar
    
    * doesn't make api call when search field is changed
    
    * add all fields option
    
    * add description as an option to search by
    
    * pagination is hidden when loading artworks
    
    * fixing centering
    
    * fix spacing
    
    * placeholders
    
    * fix loading bar spacing and clean up code
    
    * pagination count now reflects number of results
    
    * gallerypreview makes new call to API
    
    * remove console and change to single quotes
    
    * in progress
    
    * add filter bar
    
    * fix dependency warning
    
    * finish styling
    
    * nits
    
    * compute random index
    
    * in progress
    
    * fetch works, need to fix link
    
    * package-lock
    
    * fix merge error
    
    * fixed link
    
    * fixed max-len errors
    
    * remove console log
    
    * reset artworks global state when user clicks on random-btn
    
    * indhu's suggestions
    
    * fix artworks reset after user presses back button
    
    * prevents unneccessary dispatching/loading
    
    * fix max-length errors
    
    * in progress
    
    * move to museumgallery
    
    * bug fixed
    
    Co-authored-by: Isabel Sangimino <isangimino@google.com>
    Co-authored-by: Indhu Gunda <43057952+indhu-gunda@users.noreply.github.com>

--- frontend/src/components/dashboard/gallery-components/SurpriseMeButton.js
+++ frontend/src/components/dashboard/gallery-components/SurpriseMeButton.js
@@ -1,10 +1,7 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
-import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
-import {fetchMuseumArtworks, getRandomArtworkId}
-  from '../../../redux/museumArtworkActions';
-import {useDispatch, useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
@@ -16,46 +13,11 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SurpriseMeButton(
-    {numOfResults, searchQuery, sortBy, searchField, newQuery}) {
+export default function SurpriseMeButton() {
   const classes = useStyles();
-  const ARTWORKS_PER_PAGE = 9;
 
-  const [newRandomArtwork, setNewRandomArtwork] = React.useState(false);
-
-  const computeRandomIndex = (numOfResults) => {
-    const ranNum = Math.floor(Math.random() * numOfResults);
-    const pageNum = Math.floor(ranNum/ARTWORKS_PER_PAGE);
-    const index = ranNum % ARTWORKS_PER_PAGE;
-    return {pageNum, index};
-  };
-
-  const dispatch = useDispatch();
   const randomArtworkId = useSelector(
       (state) => (state.museumArtworks.randomArtworkId));
-  const [firstPgLoad, setFirstPgLoad] = React.useState(true);
-  const FIRST_PAGE = 1;
-  const EMPTY_QUERY = '';
-  useEffect(() => {
-    if (firstPgLoad) {
-      // resets artworks when users hit back button
-      dispatch(fetchMuseumArtworks(FIRST_PAGE, ARTWORKS_PER_PAGE, EMPTY_QUERY));
-      setFirstPgLoad(false);
-    }
-    if (newQuery || firstPgLoad) {
-      // newQuery makes sure that we aren't dispatching unnecessarily
-      const {pageNum, index} = computeRandomIndex(numOfResults);
-      dispatch(getRandomArtworkId(
-          pageNum, ARTWORKS_PER_PAGE, searchQuery, sortBy, searchField, index))
-          .then(setNewRandomArtwork(false));
-    }
-  },
-  [dispatch, searchQuery, sortBy, searchField,
-    numOfResults, newRandomArtwork, newQuery, firstPgLoad]);
-
-  const surpriseMe = () => {
-    setNewRandomArtwork(true);
-  };
 
   return (
     <div>
@@ -68,7 +30,6 @@ export default function SurpriseMeButton(
             variant="contained"
             color="secondary"
             className={classes.surpriseMe}
-            onClick={surpriseMe}
           >
             Surprise me!
           </Button>
@@ -78,11 +39,3 @@ export default function SurpriseMeButton(
 
   );
 }
-
-SurpriseMeButton.propTypes = {
-  numOfResults: PropTypes.number.isRequired,
-  searchQuery: PropTypes.string.isRequired,
-  sortBy: PropTypes.string.isRequired,
-  searchField: PropTypes.string.isRequired,
-  newQuery: PropTypes.string.isRequired,
-};
